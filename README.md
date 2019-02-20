4f411603131bc574b8d84351ff216329a2dc284b
Pablo Veiga <vcpablo@gmail.com>
2019-02-20 14:01:15 -0300

    ⚡️ Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## ASP.NET Core  + Firebase Cloud Messaging 
+This sample app shows a C# implementation of how to send a push notification to a registered mobile device using Firebase Cloud Messaging.
+
+### Configure and complete the code
+
+1. Configure a Firebase project and get Firebase Cloud Messaging Server Key and Sender Id
+https://firebase.google.com/docs/cloud-messaging
+
+2. Replace the keys in the code
+
+3. Import the project using Visual Studio (2017 preferred) and play it
+
+### Open on browser
+```
+http://localhost:5000/api/index
+```
---
+++ asp-net-firebase/Controllers/IndexController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace asp_net_firebase.Controllers
+{
+    [Route("api/[controller]")]
+    public class IndexController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get(string title = "notification title", string body = "notification body", int badge = 1)
+        {
+            try
+            {
+                WebRequest tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
+                tRequest.Method = "post";
+                // ServerKey - Key from Firebase cloud messaging server  
+                tRequest.Headers.Add(string.Format("Authorization: key={0}", "{ServerKey}"));
+                // SenderId - From firebase project setting  
+                tRequest.Headers.Add(string.Format("Sender: id={0}", "{SenderId}"));
+                tRequest.ContentType = "application/json";
+                // DeviceId - obtained when the device is registered
+                var payload = new
+                {
+                    to = "{DeviceId}",
+                    priority = "high",
+                    content_available = true,
+                    notification = new
+                    {
+                        body,
+                        title,
+                        badge
+                    },
+                };
+
+                string postbody = JsonConvert.SerializeObject(payload).ToString();
+                Byte[] byteArray = Encoding.UTF8.GetBytes(postbody);
+                tRequest.ContentLength = byteArray.Length;
+                using (Stream dataStream = tRequest.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    using (WebResponse tResponse = tRequest.GetResponse())
+                    {
+                        using (Stream dataStreamResponse = tResponse.GetResponseStream())
+                        {
+                            if (dataStreamResponse != null) using (StreamReader tReader = new StreamReader(dataStreamResponse))
+                                {
+                                    String sResponseFromServer = tReader.ReadToEnd();
+                                    return new string[] { sResponseFromServer };
+                                }
+                        }
+                    }
+                }
+
+                return new string[] { "No response" };
+            }
+            catch (Exception exp)
+            {
+                return new string[] { exp.Message };
+            }
+        }
+    }
+}
